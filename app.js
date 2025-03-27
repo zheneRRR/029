760ab58223bcbdb5e95b841a33b4193606e4e3ee
zheneRRR <kisa2000meow@gmail.com>
2025-03-27 22:24:19 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+<script>
+var swiper = new Swiper(".mySwiper", {
+spaceBetween: 30,
+navigation: {
+nextEl: ".swiper-button-next",
+prevEl: ".swiper-button-prev",
+},
+pagination: {
+el: ".swiper-pagination",
+},
+mousewheel: true,
+keyboard: true,
+});
+</script>
